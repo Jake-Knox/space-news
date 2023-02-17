b8ae2fac1a29a35ccae29c83f0a11d04ecda1678
jake-knox- <jakeknox11@hotmail.co.uk>
2023-02-17 23:17:52 +0000

    adding background spinning img and some divs and text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        
+        <div className="space-container">
+          <div className="space-background App-logo"></div>
+        </div>
+
+        <div className="news-container">
+          <div className="news-header">
+            <h1>The thousand world's News</h1>
+          </div>
+          <div className="news-body">
+            <div className="news-items">
+
+              <div class="news-item">
+                <h3>news in space</h3>
+                <p>news 1</p>
+              </div>
+              <div class="news-item">
+                <h3>news in space</h3>
+                <p>news 2</p>
+              </div>
+              <div class="news-item">
+                <h3>news in space</h3>
+                <p>news 3</p>
+              </div>
+              <div class="news-item">
+                <h3>news in space</h3>
+                <p>news 4</p>
+              </div>
+            </div>
+
+            <div class="sport-scores">
+              <ul>
+                <li>0 | 3</li>
+                <li>1 | 1</li>
+                <li>2 | 0</li>
+                <li>0 | 0</li>
+              </ul>
+            </div>
+
+          </div>
+        </div>      
+      </div>
     </div>
   );
 }
